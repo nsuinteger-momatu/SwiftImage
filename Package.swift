b9e005da144119c9cf4b8f06208bbcec9040df38
Mevan Alles <integerfactory@gmail.com>
2023-05-09 16:13:38 +1000

    [tech] version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftImage",
-    platforms: [.iOS(.v13), .macOS(.v12)],
+    platforms: [.iOS(.v13)],
     products: [
             .library(
                 name: "SwiftImage",
