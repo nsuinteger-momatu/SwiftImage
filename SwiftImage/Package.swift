e9765ed02c9e1d5b367c1a23cb5468bf650c3e74
Mevan Alles <integerfactory@gmail.com>
2023-05-09 15:08:21 +1000

    [tech] test

---
+++ SwiftImage/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftImage",
+    platforms: [.iOS(.v13), .macOS(.v10_14)],
+    products: [
+            .library(
+                name: "SwiftImage",
+                targets: ["SwiftImage"])
+        ],
+        dependencies: [],
+        targets: [
+            .target(
+                name: "SwiftImage",
+                dependencies: []),
+            .testTarget(
+                name: "SwiftImageTests",
+                dependencies: ["SwiftImage"]),
+        ]
+)
